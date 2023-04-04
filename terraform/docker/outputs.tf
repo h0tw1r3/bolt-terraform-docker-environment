d40cd44d0bbbeca63bd5e18bed19d650b3d40b3c
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2023-04-04 11:29:09 -0500

    improve terraform output

--- terraform/docker/outputs.tf
+++ terraform/docker/outputs.tf
@@ -1,4 +1,11 @@
-output "primary_ip" {
-    description = "primary public ip address"
-    value = values(docker_container.map)[*].network_data.0.ip_address
+output "containers" {
+  description = "containers"
+  value = {
+    for name, config in local.docker_containers:
+      name => {
+        "ip" = docker_container.map[name].network_data.0.ip_address,
+        "memory" = docker_container.map[name].memory,
+        "image" = docker_image.map[config.image].name,
+      }
+  }
 }
